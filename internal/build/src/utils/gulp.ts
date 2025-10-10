3ebb5a9503fe39141e23bfd8298eab7ecd26cb98
RAR1 <a1525730153@163.com>
2025-10-10 21:27:32 +0800

    fix: 格式化部分代码

--- internal/build/src/utils/gulp.ts
+++ internal/build/src/utils/gulp.ts
@@ -1,15 +1,14 @@
 import { buildRoot } from '@rar-ui/build-utils'
-import { run } from './process';
+import { run } from './process'
 
-import type { TaskFunction } from 'gulp';
+import type { TaskFunction } from 'gulp'
 
 export const withTaskName = <T extends TaskFunction>(name: string, fn: T) => {
-    return Object.assign(fn, { displayName: name });
+  return Object.assign(fn, { displayName: name })
 }
 
-
 export const runTask = (name: string) => {
-    return withTaskName(`shellTask:${name}`, () => 
-        run(`pnpm run start ${name}`, buildRoot)
-    )
+  return withTaskName(`shellTask:${name}`, () =>
+    run(`pnpm run start ${name}`, buildRoot),
+  )
 }
--- packages/theme/gulpfile.ts
+++ packages/theme/gulpfile.ts
@@ -1,42 +1,46 @@
 import { resolve } from 'path'
-import { src, dest, parallel, series } from 'gulp'
+import { dest, parallel, series, src } from 'gulp'
 import gulpSass from 'gulp-sass'
 import dartSass from 'sass'
 import autoprefixer from 'gulp-autoprefixer'
 import rename from 'gulp-rename'
 import { rarOutput } from '@rar-ui/build-utils'
 
-const distFolder = resolve(__dirname, 'dist');
-const distBundle = resolve(rarOutput, 'theme');
+const distFolder = resolve(__dirname, 'dist')
+const distBundle = resolve(rarOutput, 'theme')
 
 function buildThemeChalk() {
-    const sass = gulpSass(dartSass);
-    const noElPrefixFile = /(index|base|display)/
-    return src(resolve(__dirname, 'src/**/*.scss'))
+  const sass = gulpSass(dartSass)
+  const noElPrefixFile = /(index|base|display)/
+  return src(resolve(__dirname, 'src/**/*.scss'))
     .pipe(sass.sync())
     .pipe(autoprefixer({ cascade: false }))
-    .pipe(rename((path) => {
-        if(!noElPrefixFile.test(path.basename)) {
-            path.basename = `el-${path.basename}`;
+    .pipe(
+      rename((path) => {
+        if (!noElPrefixFile.test(path.basename)) {
+          path.basename = `el-${path.basename}`
         }
-    }))
-    .pipe(dest(distFolder));
+      }),
+    )
+    .pipe(dest(distFolder))
 }
 
 /**
  * 复制主题源码到dist包下
- * */ 
+ * */
 function copyThemeSource() {
-    return src(resolve(__dirname, 'src/**')).pipe(dest(resolve(distBundle, 'src')))
+  return src(resolve(__dirname, 'src/**')).pipe(
+    dest(resolve(distBundle, 'src')),
+  )
 }
 
 function copyThemeBundle() {
-    return src(resolve(__dirname, 'dist/**')).pipe(dest(distBundle));
+  return src(resolve(__dirname, 'dist/**')).pipe(dest(distBundle))
 }
 
 const build = parallel(
-    copyThemeSource,
-    series(buildThemeChalk, copyThemeBundle)
+  copyThemeSource,
+  series(buildThemeChalk, copyThemeBundle),
 )
 
-export default build;
+export default build
